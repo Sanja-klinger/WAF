e72906eb5dc69922c61a7f2c9cb420b5c45f1bed
Sanja Klinger <sladjana.klinger@gmail.com>
2024-04-23 09:44:29 +0200

    refactor: edit code

--- script.js
+++ script.js
@@ -3,8 +3,15 @@ const toggleButton = document.querySelector("button");
 toggleButton.addEventListener("click", function () {
   toggleButton.classList.toggle("dark");
   toggleButton.classList.toggle("light");
-  if (toggleButton.classList.includes("dark")) {
-    document.title = "Good night!";
-    document.body.style.backgroundColor = black;
+  if (toggleButton.classList.contains("dark")) {
+    document.title = "Laku noć!";
+    document.body.style.transition = "background-color 0.5s ease";
+    document.body.style.backgroundColor = "black";
+    toggleButton.style.borderColor = "white";
+  } else {
+    document.title = "Good morning!";
+    document.body.style.transition = "background-color 0.5s ease";
+    document.body.style.backgroundColor = "white";
+    toggleButton.style.borderColor = "black";
   }
 });
