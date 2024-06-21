30da8be892b81484a48f883382124536ceee9d9e
Sanja Klinger <sladjana.klinger@gmail.com>
2024-06-21 09:26:33 +0200

    chore: type code

--- todo/script.js
+++ todo/script.js
@@ -3,19 +3,32 @@ const btn = document.getElementById("btn");
 const toDoList = document.querySelector("#todoList");
 const doneBtn = document.getElementById("doneBtn");
 
+/*Inside the curly braces{} that define an object literal, you define properties as key-value pairs.
+todos: [] initializes a property named todos with an empty array as its value.
+filter: 'All' initializes a property named filter with the string 'All' as its value.*/
 const state = {
   todos: [],
+  filter: "All",
 };
 
 function render() {
-  // Clear the current list
+  // Clear the current list, so we don't have the previous list repeated
   toDoList.innerHTML = "";
-  state.todos.forEach((notes) => {
+
+  // Filter todos based on the current filter state: if 'all' return function, if done return done, if open return all NOT done
+  const filteredTodos = state.todos.filter((notes) => {
+    if (state.filter === "All") return true;
+    if (state.filter === "Done") return notes.done;
+    if (state.filter === "Open") return !notes.done;
+  });
+
+  filteredTodos.forEach((notes) => {
     const li = document.createElement("li");
 
     const checkBox = document.createElement("input");
     checkBox.type = "checkbox";
     checkBox.checked = notes.done;
+    //Ensuring that each checkbox has a unique id, which is important for accessibility, as well as for label element`s for attribute
     checkBox.id = `checkbox-${notes.id}`;
 
     const label = document.createElement("label");
@@ -71,8 +84,6 @@ document.getElementById("todoForm").addEventListener("submit", (event) => {
 
 doneBtn.addEventListener("click", (event) => {
   event.preventDefault(); // Prevent form submission
-
-  //todos = [...newSet(todos)]; wrong approach
 
   // Filter out the completed todos
   state.todos = state.todos.filter((notes) => !notes.done);
